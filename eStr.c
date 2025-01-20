3df86a7410a44756bace206afc7e04a8b97eccd8
Esteban <acevedoesteban999@gmail.com>
2025-01-20 17:43:37 -0500

    UPD: use capacity UPD: Use Optimize By Memory

--- eStr.c
+++ eStr.c
@@ -1,32 +1,51 @@
 #include "eStr.h"
 
+size_t OPTIMIZE_LEN = DEFAULT_OPTIMIZE_LEN;
 
-void estr_init(eStr *str) {
-    str->data = NULL;
+typedef struct {
+    char *data;    
+    size_t length; 
+    size_t capacity;
+} eStr;
+
+
+void estr_free(eStr *str) {
+    if (str->data != NULL) {
+        free(str->data); 
+        str->data = NULL;
+    }
     str->length = 0;
+    str->capacity = 0;
 }
 
 bool estr_copy(eStr *str, const char *text) {
     estr_free(str);
-    return estr_append_str(str,text);
+    return estr_append_str(str,true, text);
 }
 
-bool estr_append_str(eStr *str, const char *text) {
+bool estr_append_str(eStr *str, bool is_optimized_for_memory, const char *text ) {
     size_t text_len = strlen(text);
     size_t new_len = str->length + text_len;
 
-    char *new_data = realloc(str->data, new_len + 1); 
-    if (!new_data) {
-        return false; 
+    if (new_len + 1 > str->capacity) {
+
+        size_t new_capacity = new_len + ( is_optimized_for_memory ? 0 : OPTIMIZE_LEN ) + 1;
+        
+        char *new_data = realloc(str->data, new_capacity); 
+        if (!new_data) {
+            return false; 
+        }
+
+        str->data = new_data;
+        str->capacity = new_capacity;
     }
 
-    memcpy(new_data + str->length, text, text_len + 1);
-    str->data = new_data;
+    memcpy(str->data + str->length, text, text_len + 1); 
     str->length = new_len;
     return true;
 }
 
-bool estr_append_format(eStr *str, const char *format, ...) {
+bool estr_append_format(eStr *str,bool is_optimized_for_memory, const char *format, ...) {
     va_list args;
     va_start(args, format);
 
@@ -36,28 +55,16 @@ bool estr_append_format(eStr *str, const char *format, ...) {
         return false;
     }
 
-    char *buff = (char *)malloc(size + 1);
+    char *buff = (char *)malloc(size + 1); 
     if (!buff) {
         va_end(args);
-        return false;
+        return false; 
     }
 
     vsnprintf(buff, size + 1, format, args);
-
     va_end(args);
-
-    bool res = estr_append_format(str, buff);
-
+    
+    bool res = estr_append_str(str, is_optimized_for_memory, buff);
     free(buff);
-
     return res;
 }
-
-void estr_free(eStr *str) {
-    if (str->data != NULL) {
-        free(str->data); 
-        str->data = NULL;
-    }
-    str->length = 0;
-}
-
--- include/eStr.h
+++ include/eStr.h
@@ -5,17 +5,19 @@
 #include <stdbool.h>
 #include <stdarg.h>
 
+#define DEFAULT_OPTIMIZE_LEN 256
+
 typedef struct {
     char *data;     
     size_t length;  
 } eStr;
 
-void estr_init(eStr *str);
+extern size_t OPTIMIZE_LEN;
 
 bool estr_copy(eStr *str, const char *text);
 
-bool estr_append_str(eStr *str, const char *text);
+bool estr_append_str(eStr *str, bool is_optimized_for_memory , const char *text);
 
-bool estr_append_format(eStr *str, const char *format, ...);
+bool estr_append_format(eStr *str,bool is_optimized_for_memory,const char *format, ...);
 
 void estr_free(eStr *str);
