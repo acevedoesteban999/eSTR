94abf950ac34c729aa8c3cc09731ee9e7cbe30f0
Esteban <acevedoesteban999@gmail.com>
2025-01-20 22:56:19 -0500

    .

--- include/eStr.h
+++ include/eStr.h
@@ -8,13 +8,16 @@
 #define DEFAULT_OPTIMIZE_LEN 256
 
 typedef struct {
-    char *data;     
-    size_t length;  
+    char *data;    
+    size_t length; 
+    size_t capacity;
 } eStr;
 
 extern size_t OPTIMIZE_LEN;
 
-bool estr_copy(eStr *str, const char *text);
+bool estr_copy_str(eStr *str, const char *text);
+
+bool estr_copy_format(eStr *str, const char *format, ...);
 
 bool estr_append_str(eStr *str, bool is_optimized_for_memory , const char *text);
 
